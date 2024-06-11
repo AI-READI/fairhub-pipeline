dc1db825cf2000d0c3c0ff4152a99e3e5477a211
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2024-06-10 20:49:00 -0700

    ✨ feat: add ecg process pipeline

--- stage_one/ecg_pipeline.py
+++ stage_one/ecg_pipeline.py
@@ -1,31 +1,24 @@
 """Process ecg data files"""
 
+import contextlib
 import datetime
-import json
 import os
 import tempfile
-import uuid
-
+import shutil
+import ecg.ecg_root as ecg
 import azure.storage.blob as azureblob
 import azure.storage.filedatalake as azurelake
-
 import config
 
 
-def pipeline():
-    """Reads the data in the stage-1-container. Each file name is added to a log file in the logs folder for the study.
-    Will also create an output file with a modified name to simulate a processing step.
-    POC so this is just a test to see if we can read the files in the stage-1-container.
-    """
+def pipeline(study_id: str):
 
-    def data_identifier(file):
-        if "ENV" in file and file.endswith(".zip"):
-            return "Environmental Sensor File"
-        else:
-            return "Unknown File Type"
+    if study_id is None or not study_id:
+        raise ValueError("study_id is required")
 
-    input_folder = "AI-READI/pooled-data/EnvSensor"
-    logs_folder = "AI-READI/logs/"
+    input_folder = f"{study_id}/pooled-data/ECG-pool"
+    processed_data_output_folder = f"{study_id}/pooled-data/ECG-processed"
+    data_plot_output_folder = f"{study_id}/pooled-data/ECG-dataplot"
 
     sas_token = azureblob.generate_account_sas(
         account_name="b2aistaging",
@@ -42,14 +35,18 @@ def pipeline():
         credential=sas_token,
     )
 
-    # Create a temporary folder for this workflow
-    workflow_id = uuid.uuid4()
-
     # Get the list of blobs in the input folder
     file_system_client = azurelake.FileSystemClient.from_connection_string(
         config.AZURE_STORAGE_CONNECTION_STRING,
         file_system_name="stage-1-container",
     )
+
+    # Delete the output folder if it exists
+    with contextlib.suppress(Exception):
+        file_system_client.delete_directory(processed_data_output_folder)
+
+    with contextlib.suppress(Exception):
+        file_system_client.delete_directory(data_plot_output_folder)
 
     paths = file_system_client.get_paths(path=input_folder)
 
@@ -59,44 +56,121 @@ def pipeline():
         t = str(path.name)
         str_paths.append(t)
 
-    # generate temp file for logs
-    temp_log_file, temp_log_file_path = tempfile.mkstemp(suffix=".env.log")
+    # Create a temporary folder on the local machine
+    temp_folder_path = tempfile.mkdtemp()
 
-    extracted_metadata = []
+    # Create the output folder
+    file_system_client.create_directory(processed_data_output_folder)
 
-    for path in str_paths:
+    for idx, path in enumerate(str_paths):
+        log_idx = idx + 1
+
+        file_client = file_system_client.get_file_client(path)
+        is_directory = file_client.get_file_properties().metadata.get("hdi_isfolder")
+
+        if is_directory:
+            continue
+
+        print(f"Processing {path} - ({log_idx}/{len(str_paths)})")
+
         # get the file name from the path
         file_name = path.split("/")[-1]
 
-        env_sensor_file = data_identifier(file_name)
+        # skip if the path is a folder (check if extension is empty)
+        if not path.split(".")[-1]:
+            continue
 
-        if env_sensor_file == "Environmental Sensor File":
-            # split the path name by the - character
-            components = file_name.split("_")
-            # extract the metadata from the file name
-            extracted_metadata.append(
-                {
-                    "file_name": file_name,
-                    "site_name": components[0],
-                    "data_type": components[1],
-                    "site_name_2": components[2],
-                    "data_type_2": components[3],
-                    "date_range": components[4],
-                    "prefix": components[5].split("-")[0],
-                    "patient_id": components[5].split("-")[1],
-                    "sensor_id": os.path.splitext(components[5].split("-")[2])[0],
-                }
-            )
+        # download the file to the temp folder
+        blob_client = blob_service_client.get_blob_client(
+            container="stage-1-container", blob=path
+        )
 
-    # write the paths to the log file
-    with open(temp_log_file_path, mode="w", encoding="utf-8") as f:
-        formatted_text = json.dumps(extracted_metadata, indent=4)
-        f.write(formatted_text)
+        download_path = os.path.join(temp_folder_path, file_name)
 
-    # upload the log file to the logs folder
-    log_blob_client = blob_service_client.get_blob_client(
-        container="stage-1-container", blob=f"{logs_folder}{workflow_id}.env.log"
-    )
+        with open(download_path, "wb") as data:
+            blob_client.download_blob().readinto(data)
 
-    with open(temp_log_file_path, "rb") as data:
-        log_blob_client.upload_blob(data)
+        print(
+            f"Downloaded {file_name} to {download_path} - ({log_idx}/{len(str_paths)})"
+        )
+
+        ecg_path = download_path
+
+        ecg_temp_folder_path = tempfile.mkdtemp()
+        wfdb_temp_folder_path = tempfile.mkdtemp()
+
+        xecg = ecg.ECG()
+
+        conv_retval_dict = xecg.convert(
+            ecg_path, ecg_temp_folder_path, wfdb_temp_folder_path
+        )
+
+        print(f"Converted {file_name} - ({log_idx}/{len(str_paths)})")
+
+        output_files = conv_retval_dict["output_files"]
+
+        print(
+            f"Uploading {file_name} to {processed_data_output_folder} - ({log_idx}/{len(str_paths)}"
+        )
+
+        for file in output_files:
+            with open(f"{file}", "rb") as data:
+                file_name = file.split("/")[-1]
+                output_blob_client = blob_service_client.get_blob_client(
+                    container="stage-1-container",
+                    blob=f"{processed_data_output_folder}/{file_name}",
+                )
+                output_blob_client.upload_blob(data)
+
+        print(
+            f"Uploaded {file_name} to {processed_data_output_folder} - ({log_idx}/{len(str_paths)})"
+        )
+
+        # Do the data plot
+        print(f"Data plotting {file_name} - ({log_idx}/{len(str_paths)})")
+
+        dataplot_retval_dict = xecg.dataplot(conv_retval_dict, ecg_temp_folder_path)
+
+        print(f"Data plotted {file_name} - ({log_idx}/{len(str_paths)})")
+
+        dataplot_pngs = dataplot_retval_dict["output_files"]
+
+        print(
+            f"Uploading {file_name} to {data_plot_output_folder} - ({log_idx}/{len(str_paths)}"
+        )
+
+        for file in dataplot_pngs:
+            with open(f"{file}", "rb") as data:
+                file_name = file.split("/")[-1]
+                output_blob_client = blob_service_client.get_blob_client(
+                    container="stage-1-container",
+                    blob=f"{data_plot_output_folder}/{file_name}",
+                )
+                output_blob_client.upload_blob(data)
+
+        print(
+            f"Uploaded {file_name} to {data_plot_output_folder} - ({log_idx}/{len(str_paths)}"
+        )
+
+        # Create the file metadata
+
+        print(f"Creating metadata for {file_name} - ({log_idx}/{len(str_paths)})")
+
+        output_hea_file = conv_retval_dict["output_hea_file"]
+
+        hea_metadata = xecg.metadata(output_hea_file)
+        print(hea_metadata)
+
+        print(f"Metadata created for {file_name} - ({log_idx}/{len(str_paths)})")
+
+        shutil.rmtree(ecg_temp_folder_path)
+        shutil.rmtree(wfdb_temp_folder_path)
+        os.remove(download_path)
+
+
+if __name__ == "__main__":
+    pipeline()
+
+    # delete the ecg.log file
+    if os.path.exists("ecg.log"):
+        os.remove("ecg.log")
