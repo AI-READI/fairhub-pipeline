b4e86c7bd428c6dab683a84463550ae3993ddce3
aydawka <aydan.gasimova2@gmail.com>
2024-08-06 13:21:30 -0700

    fix: typo

--- function_app.py
+++ function_app.py
@@ -27,7 +27,7 @@ from publish_pipeline.generate_high_level_metadata.generate_study_description im
     pipeline as generate_study_description_pipeline,
 )
 from publish_pipeline.register_doi.register_doi import pipeline as register_doi_pipeline
-from stage_one.ecg_pipeline import pipeline as stage_one_ecg_pipeline
+from ecg_pipeline import pipeline as stage_one_ecg_pipeline
 from eidon_pipeline import pipeline as stage_one_eidon_pipeline
 from stage_one.env_sensor_pipeline import pipeline as stage_one_env_sensor_pipeline
 from stage_one.img_identifier_pipeline import (
