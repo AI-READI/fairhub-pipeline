8604fb19ea194890a74dbd6f6e1ac1b5366e10d2
aydawka <aydan.gasimova2@gmail.com>
2024-08-09 13:49:37 -0700

    fix: comment loop

--- triton_pipeline.py
+++ triton_pipeline.py
@@ -128,8 +128,8 @@ def pipeline(study_id: str):  # sourcery skip: low-code-quality
         log_idx = idx + 1
 
         # dev
-        if log_idx == 10:
-            break
+        # if log_idx == 15:
+        #     break
 
         # Create a temporary folder on the local machine
         temp_folder_path = tempfile.mkdtemp()
