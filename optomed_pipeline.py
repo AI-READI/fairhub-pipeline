b435e5009e45d623291c0bd05f3d26610f732003
aydawka <aydan.gasimova2@gmail.com>
2024-09-04 20:58:59 -0700

    wip: set protocols

--- optomed_pipeline.py
+++ optomed_pipeline.py
@@ -123,8 +123,8 @@ def pipeline(study_id: str):  # sourcery skip: low-code-quality
         log_idx = idx + 1
 
         # dev
-        if log_idx == 100:
-            break
+        # if log_idx == 10:
+        #     break
 
         # Create a temporary folder on the local machine
         temp_folder_path = tempfile.mkdtemp()
