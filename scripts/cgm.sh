6848bc8a2dd54ffe2fcdbdeac01f4b0803059148
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2025-02-12 12:08:51 -0800

    👷 ci: update cgm script

--- scripts/cgm.sh
+++ scripts/cgm.sh
@@ -3,7 +3,8 @@
 # Set the directory paths
 PROJECT_DIR="/home/azureuser/fairhub-pipeline"  
 VENV_DIR="$PROJECT_DIR/.venv"
-PYTHON_SCRIPT="$PROJECT_DIR/cgm_pipeline.py --workers 8"    
+PYTHON_SCRIPT="$PROJECT_DIR/cgm_pipeline.py"    
+ARGS="--workers 8"
 
 # Go to project directory
 cd "$PROJECT_DIR" || exit
@@ -12,7 +13,7 @@ cd "$PROJECT_DIR" || exit
 source "$VENV_DIR/bin/activate"
 
 # Run the Python script
-python3 "$PYTHON_SCRIPT"
+python3 "$PYTHON_SCRIPT" $ARGS
 
 # Deactivate virtual environment
 deactivate
